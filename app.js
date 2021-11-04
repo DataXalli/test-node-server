012d6b6bb36331027ac2eebd83e41f10eb6a1638
Data Xalli <valther.rodriguez@outlook.com>
2021-11-04 10:12:05 -0600

    Heroku deploy

--- app.js
+++ app.js
@@ -25,7 +25,8 @@ app.post('/imagen', upload.single('imagen'),async (req, res) => {
     res.send({resizedImage: processedImage});
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Server is running on port', PORT);
 });
 
