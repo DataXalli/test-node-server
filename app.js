3aae9a42a27fb1e2a96535cc37ba59c0a2440144
Data Xalli <valther.rodriguez@outlook.com>
2021-11-03 23:59:23 -0600

    Second Commit

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const multer = require('multer');
+const sharp = require('sharp');
+const fs = require('fs');
+
+const storageStrategy = multer.memoryStorage();
+const upload = multer({ storage: storageStrategy });
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+
+app.post('/imagen', upload.single('imagen'),async (req, res) => {
+    const body = req.body;
+    const imagen = req.file;
+    const processedImage = await sharp(imagen.buffer).resize(300, 300,{
+        fit: sharp.fit.contain,
+        background: { r: 255, g: 255, b: 255, alpha: 0 }
+    }).toBuffer();
+    fs.writeFileSync(`./uploads/${body.nombre}.jpg`, processedImage);
+    console.log(processedImage);
+    res.send({resizedImage: processedImage});
 });
 
 app.listen(3000, () => {
